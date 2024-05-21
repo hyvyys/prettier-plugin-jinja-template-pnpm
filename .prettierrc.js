d2e4ee6fc91590fa1d02857ea1c13966286a0027
Adam Jagosz <adamjagosz@gmail.com>
2024-05-21 18:11:01 +0200

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: ['prettier-plugin-jinja-template'],
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 120,
+  endOfLine: 'auto',
+  arrowParens: 'avoid',
+};
